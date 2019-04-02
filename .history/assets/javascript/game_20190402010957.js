4f760ebde653956e4b8ec2f335d180d8832d37c1
dsaftler <dsaftler@att.net>
2019-04-02 01:19:28 -0400

    layout complete, game.js started

---
+++ .history/assets/javascript/game_20190402010957.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+$(".crystal").click(function(){
+    alert("You clicked crystal 0");
+  // $("#crystal_2").click(addPoints(this));
+  // $("#crystal_3").click(addPoints(this));
+  // $("#crystal_4").click(addPoints(this));
+  //add (x) points to total #id="crystal_0"
+  // show total points
+  // if points<random
+  // return
+  // if if points===random
+  //    win game?
+  //    increment wins
+  // else
+  //    lose game?
+  //    increment losses
+  //  initGame()
+
+});
+});
+
+function initGame(){
+  //for crystals 0-3
+  // assign random number 1-12
+  // randomNumber assign random number 19-120
+  // points=0
+  // show wins, show losses, show points, show random
+}
